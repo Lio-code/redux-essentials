0a9f6c5111022c02a228fe76fc87bd7ad53976aa
Lionel <lio4455@hotmail.com>
2022-04-19 20:08:44 +0200

    ajax call with redux thunk to diplay postsList with the fake api

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,5 +1,6 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
 import { sub } from 'date-fns'
+import { client } from '../../api/client'
 
 const myReactions = { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 }
 
@@ -8,6 +9,11 @@ const initialState = {
   status: 'idle',
   error: null,
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
 
 //the reducer "postAdded" is a special kind with one more section: "prepare". This section is usefull to prepare some data that will need to be used but that will be absent in the initial action dispatched. Here, "prepare" will prepare data for id, date and reactions. The rest (title, content, userId) won't need to be prepared as it will come from the action.payload.
 
@@ -49,6 +55,21 @@ const postsSlice = createSlice({
         existingPost.reactions[reaction]++
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
   },
 })
 
