26ed50e84b296cedcf4f505ff26691761b6b9fe7
Lionel <lio4455@hotmail.com>
2022-05-28 16:09:25 +0200

    logic in navbar

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Link } from 'react-router-dom'
 
-import { fetchNotifications } from '../features/notifications/notificationsSlice'
+import {
+  fetchNotifications,
+  selectAllNotifications,
+} from '../features/notifications/notificationsSlice'
 
 export const Navbar = () => {
   const dispatch = useDispatch()
+  const notifications = useSelector(selectAllNotifications)
+  const numUnreadNotifications = notifications.filter((n) => !n.read).length
 
   const fetchNewNotifications = () => {
     dispatch(fetchNotifications())
